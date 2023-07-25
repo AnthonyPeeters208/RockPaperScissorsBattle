26a9ea3b8f119ba229a848d87b916b8a52812433
Anthony <peetersanthony208@gmail.com>
2023-07-25 20:17:09 +0200

    Added simple white canvas, modified colours

--- js_src/main.js
+++ js_src/main.js
@@ -1,1 +1,8 @@
-document.getElementById("test").innerHTML = "Test succeeded :)"
+document.getElementById("test").innerHTML = "Test succeeded :)";
+
+// Change battle canvas style
+let canvas_dim = Math.min(screen.width, screen.height) * 0.7;
+console.log("Canvas dim: " + canvas_dim);
+let battle_canvas = document.getElementById("battlecanvas");
+battle_canvas.width = canvas_dim;
+battle_canvas.height = canvas_dim;
