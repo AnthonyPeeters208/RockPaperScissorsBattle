7c1329603ebbd40d650469ec0e1293968bed41da
Anthony <peetersanthony208@gmail.com>
2023-07-26 22:55:08 +0200

    Added random generation + counting of individual occurences

--- js_src/main.js
+++ js_src/main.js
@@ -12,24 +12,63 @@ const battle_canvas = document.getElementById("battlecanvas");
 battle_canvas.width = canvas_dim;
 battle_canvas.height = canvas_dim;
 
-// map the start button
-const startbutton = document.getElementById("startbutton");
-function clickstartbutton(){
-    alert("This doesn't work yet c:");
-}
-startbutton.addEventListener("click", clickstartbutton);
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function minusoneorone(){
+    // randomly selects either -1 or 1
+    let choice = getRandomInt(2);
+    if(choice === 0){ return 1}
+    else{ return -1}
+}
 
-const itemsRPS = [];
-itemsRPS.push(new Rock(getRandomInt(canvas_dim), getRandomInt(canvas_dim), 2, 2));
-itemsRPS.push(new Rock(getRandomInt(canvas_dim), getRandomInt(canvas_dim), -2, 2));
-itemsRPS.push(new Paper(getRandomInt(canvas_dim), getRandomInt(canvas_dim), -1, -1));
-itemsRPS.push(new Paper(getRandomInt(canvas_dim), getRandomInt(canvas_dim), 1, 1));
-itemsRPS.push(new Scissors(getRandomInt(canvas_dim), getRandomInt(canvas_dim), 1, 2));
+let itemsRPS = [];
+
+function spawnItems(){
+    itemsRPS = [];  // clear all items
+    let amount = document.getElementById("slidervalue").innerHTML;
+
+    // 3x25% for each type, final 25% is random
+    for(let j=0; j < amount; j++){
+        let xspeed = minusoneorone() + (minusoneorone() * Math.random()) ;   // some number [-2, 2]
+        let yspeed = minusoneorone() + (minusoneorone() * Math.random());
+        console.log(xspeed + " and " + yspeed);
+
+        if(j <= Math.floor(amount * 0.25)){
+            itemsRPS.push(new Scissors(getRandomInt(canvas_dim), getRandomInt(canvas_dim), xspeed, yspeed));
+        }
+        else if(j <= Math.floor(amount * 0.5)){
+            itemsRPS.push(new Paper(getRandomInt(canvas_dim), getRandomInt(canvas_dim), xspeed, yspeed));
+        }
+        else if(j <= Math.floor(amount * 0.75)){
+            itemsRPS.push(new Rock(getRandomInt(canvas_dim), getRandomInt(canvas_dim), xspeed, yspeed));
+        }
+        else{
+            // do a random spawn
+            let choice = getRandomInt(3);
+            if(choice === 0){itemsRPS.push(new Rock(getRandomInt(canvas_dim), getRandomInt(canvas_dim), xspeed, yspeed));}
+            if(choice === 1){itemsRPS.push(new Paper(getRandomInt(canvas_dim), getRandomInt(canvas_dim), xspeed, yspeed));}
+            if(choice === 2){itemsRPS.push(new Scissors(getRandomInt(canvas_dim), getRandomInt(canvas_dim), xspeed, yspeed));}
+        }
+    }
+    updateAmounts();    // update
+}
+
+function updateAmounts(){
+    let rocks = 0;
+    let papers = 0;
+    let scissors = 0;
+    for(let i=0; i<itemsRPS.length; i++){
+        if(itemsRPS[i] instanceof Rock){rocks+=1}
+        else if(itemsRPS[i] instanceof Paper){papers+=1}
+        else if(itemsRPS[i] instanceof Scissors){scissors+=1}
+    }
+    document.getElementById("rock_amount").innerHTML = rocks;
+    document.getElementById("paper_amount").innerHTML = papers;
+    document.getElementById("scissors_amount").innerHTML = scissors;
+}
+
 
 function moveAllItems(){
     const ctx = document.getElementById("battlecanvas").getContext("2d");
@@ -44,6 +83,10 @@ function moveAllItems(){
     }
     window.requestAnimationFrame(moveAllItems);
 }
-window.requestAnimationFrame(moveAllItems); // initial call
+// Initial
+spawnItems();
+window.requestAnimationFrame(moveAllItems);
 
-
+// map the start button
+const startbutton = document.getElementById("startbutton");
+startbutton.addEventListener("click", spawnItems);
